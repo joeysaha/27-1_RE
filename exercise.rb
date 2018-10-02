1718b0f2f07c10102a8b62b7507cbe3b60174c88
Joey Saha <joeysaha@hotmail.com>
2018-10-02 17:45:09 -0400

    works

---
+++ exercise.rb
@@ -0,0 +1,15 @@
+a = [
+  {id: "38fj8d900", city: 'Hamilton', events: [{date: '2017-01-01', attendees: 100}, {date: '2016-12-31', attendees: 60}]},
+  {id: "39fo837y7", city: 'Toronto', events: [{date: '2017-03-30', attendees: 3000}, {date: '2017-07-07', attendees: 2500}, {date: '2017-02-04', attendees: 900}]},
+  {id: "58uj8d800", city: 'Montreal', events: [{date: '2017-08-10', attendees: 250}]},
+  {id: "48hn8d900", city: 'Kingston', events: [{ date: '2015-04-16', attendees: 45}]}
+]
+
+a.each do |element|
+  puts element[:city]
+  puts "-----------"
+  element[:events].each do |event|
+    puts "Date: #{event[:date]}, #{event[:attendees]} people"
+  end
+  puts "\n"
+end
